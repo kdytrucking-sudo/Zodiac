3556414a5a7708870612ff826f4ed815f7d0de3f
kdytrucking <kdytrucking@gmail.com>
2025-11-30 15:27:40 +0000

    tes

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Zodiac app is running!");
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDBk4Qspp1eBT1rkUhmffWLf4a4kAF26gU",
+  authDomain: "studio-4395392521-1abeb.firebaseapp.com",
+  projectId: "studio-4395392521-1abeb",
+  storageBucket: "studio-4395392521-1abeb.firebasestorage.app",
+  messagingSenderId: "413532569115",
+  appId: "1:413532569115:web:afa287880769758b5382be"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
